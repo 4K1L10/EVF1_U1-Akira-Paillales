724feef4b46195cb3a7ddfb6cfa8279c307c9a38
4K1L10 <akira.paillalef.hidalgo@cftmail.cl>
2024-04-30 00:58:47 -0400

    Subira de EVF1_U1

---
+++ MercyDevelopers/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MercyDevelopers.Models;
+
+public partial class Cliente
+{
+    public int IdCliente { get; set; }
+
+    public string? Rut { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? IdCod { get; set; }
+}
---
+++ MercyDevelopers/Models/MercyDevelopersContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace MercyDevelopers.Models;
+
+public partial class MercyDevelopersContext : DbContext
+{
+    public MercyDevelopersContext()
+    {
+    }
+
+    public MercyDevelopersContext(DbContextOptions<MercyDevelopersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente).HasName("PRIMARY");
+
+            entity.ToTable("cliente");
+
+            entity.Property(e => e.IdCliente)
+                .ValueGeneratedNever()
+                .HasColumnType("int(11)");
+            entity.Property(e => e.Apellido).HasMaxLength(45);
+            entity.Property(e => e.Correo).HasMaxLength(45);
+            entity.Property(e => e.Direccion).HasMaxLength(45);
+            entity.Property(e => e.IdCod)
+                .HasMaxLength(45)
+                .HasColumnName("ID_Cod");
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+            entity.Property(e => e.Rut).HasMaxLength(45);
+            entity.Property(e => e.Telefono).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
